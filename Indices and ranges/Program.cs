8639d14bcc1f68316dd89f8e8329ece67c9fef50
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-02-14 21:37:22 +0100

    feat:added indices and ranges, methods and ways

--- Indices and ranges/Program.cs
+++ Indices and ranges/Program.cs
@@ -87,6 +87,39 @@ namespace indicesandranges
                     Console.Write($"{cell},");
                 }
             }
+
+            int[] sequence = Sequence(1000);
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = start..(start + 10);
+                var (min, max, average) = MovingAverage(sequence, r);
+
+                Console.WriteLine($"From {r.Start} to {r.End}: \tMin :{min},tMax:{max}, tAverage:{average}");
+            }
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = ^(start + 10)..^start;
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:  \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+            (int min, int max, double Average) MovingAverage(int[] subSequence, Range range) =>
+                (
+                subSequence[range].Min(),
+                subSequence[range].Max(),
+                subSequence[range].Average()
+                );
+            int[] Sequence(int count) => Enumerable.Range(0, count).Select(x => (int)(Math.Sqrt(x) * 100)).ToArray();
+
+            var arrayOfFiveItems = new[] { 1, 2, 3, 4, 5 };
+
+            var firstThreeItems = arrayOfFiveItems[..3];
+
+            firstThreeItems[0] = 11;
+
+            Console.WriteLine(string.Join(",", firstThreeItems));
+            Console.WriteLine(string.Join(",", arrayOfFiveItems));
         }
     }
 }
