f39852d4f4954f2dcf910fca48db669775330b9b
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-02-19 11:26:23 +0100

    feat:added feature to type a character line by line synchronously

---
+++ Basic-Console-Application/Program.cs
@@ -0,0 +1,48 @@
+﻿//https:\//learn.microsoft.com/en-us/dotnet/csharp/tutorials/console-teleprompter
+namespace TeleprompterConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        var lines = ReadFrom("sampleQuotes.txt");
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+            if(!string.IsNullOrWhiteSpace(line))
+            {
+                var pause = Task.Delay(200);
+                //Synchronously waiting for a task to complete execution, 
+                //this is an anti-pattern
+                pause.Wait();
+            }
+        }
+    }
+    static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+        using (var reader = File.OpenText("/Users/ibukunoluwaakintobi/Documents/sampleQuotes.txt"))
+        {
+            while((line = reader.ReadLine()) != null)
+            //The object returned by the ReadFrom method contains the code to generate each item in the sequence
+            // here, this involcves reading one line from the text file and returning it 
+            {
+                //yield return line;
+                var words = line.Split(' ');
+                var lineLength = 0;
+                foreach (var word in words)
+                {
+                    yield return word + " ";
+                    lineLength += word.Length + 1;
+                    if(lineLength > 70)
+                    {
+                        yield return Environment.NewLine;
+                        lineLength = 0;
+                    }
+                }
+                yield return Environment.NewLine;
+            }
+        }
+    }
+}
